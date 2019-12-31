ff4a957572499b32c896101157fa82bd484962bf
Adam Longman <boardhead89@gmail.com>
2019-12-31 19:11:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,9 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto => :known_for, :languages]
-programmer_hash[:yukihiro_matsumoto][:known_for => "Ruby"]
-programmer_hash[:yukihiro_matsumoto][:languages => ["LISP", "C"]]
+programmer_hash[:yukihiro_matsumoto => :known_for=> "Ruby", :languages => ["LISP", "C"]]
 return programmer_hash
 end
 
